562db2281a312d93d4c572be2ce15f2f7b246db1
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-10-10 17:28:42 +0100

    Add user validations and create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+	validates :username, presence: true, 
+												uniqueness: true,
+												length: { in: 4..12 }
+	validates :password, presence: true,
+												length: { in: 6..16 }
 end

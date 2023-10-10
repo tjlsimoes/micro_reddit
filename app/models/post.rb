1f162a172010d4bc6cfc9734c0283f7c87c91d71
tjlsimoes <118819479+tjlsimoes@users.noreply.github.com>
2023-10-10 18:38:47 +0100

    Add Comment model and associations with User and Post
    Create instances for testing

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  has_many :comments
+	belongs_to :user
 
 	validates :title, presence: true, 
 												uniqueness: true,
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 	has_many :posts
-	
+	has_many :comments
+
 	validates :username, presence: true, 
 												uniqueness: true,
 												length: { in: 4..12 }
